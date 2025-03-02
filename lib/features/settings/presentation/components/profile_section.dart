9965cd0bd2b1916230ce66d66740428f07fed0ba
KenStarry <starrycodes@gmail.com>
2025-03-02 15:49:04 +0300

    Profile Section implementation

---
+++ lib/features/settings/presentation/components/profile_section.dart
@@ -0,0 +1,71 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sliver_tools/sliver_tools.dart';
+
+import '../../../../core/presentation/bloc/user_bloc.dart';
+
+class ProfileSection extends StatefulWidget {
+  const ProfileSection({super.key});
+
+  @override
+  State<ProfileSection> createState() => _ProfileSectionState();
+}
+
+class _ProfileSectionState extends State<ProfileSection> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, userState) {
+        return SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+          sliver: MultiSliver(children: [
+            SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 16,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: const EdgeInsets.only(top: 30),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Theme.of(context).colorScheme.onSecondary),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: userState is UserSuccess &&
+                            userState.user.userAvatar != null
+                            ? CachedNetworkImage(
+                          imageUrl: userState.user.userAvatar!,
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                        )
+                            : UnconstrainedBox(
+                          child: SvgPicture.asset(
+                              "assets/images/icons/user.svg",
+                              width: 24,
+                              height: 4,
+                              colorFilter: ColorFilter.mode(
+                                  Theme.of(context)
+                                      .textTheme
+                                      .bodySmall!
+                                      .color!,
+                                  BlendMode.srcIn)),
+                        ),
+                      ),
+                    ),
+                    Text(
+                        userState is UserSuccess ? userState.user.userName : 'User',
+                        style: Theme.of(context).textTheme.titleMedium),
+                  ],
+                ))
+          ]),
+        );
+      },
+    );
+  }
+}
